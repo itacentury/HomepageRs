f7fa8e8f1fc171d66ff1a4577d8979887747d344
Julian Hoffmann <julian.hoffmann@outlook.com>
2025-05-02 10:39:59 +0200

    added error handling for 500 & 404

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::Request;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,7 +7,17 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Whoops! Looks like we messed up."
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index]).register("/", catchers![internal_error, not_found])
 }
