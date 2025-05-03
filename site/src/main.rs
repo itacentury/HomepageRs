f690f03e1848795a166d1e5bde26c6705ba2757e
Julian <bycentury@gmail.com>
2025-05-03 13:49:57 +0200

    added separate pages for errors 404 and 500

--- site/src/main.rs
+++ site/src/main.rs
@@ -64,13 +64,15 @@ fn get_experience_vec() -> Vec<serde_json::Value> {
 }
 
 #[catch(500)]
-fn internal_error() -> &'static str {
-    "Whoops! Looks like we messed up."
+fn internal_error() -> Template {
+    Template::render("error/500", context! {})
 }
 
 #[catch(404)]
-fn not_found(req: &Request) -> String {
-    format!("I couldn't find '{}'. Try something else?", req.uri())
+fn not_found(req: &Request) -> Template {
+    Template::render("error/404", context! {
+        uri: req.uri()
+    })
 }
 
 #[launch]
